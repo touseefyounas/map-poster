6aa6558372fb018dedc8a66a018c95ecec6dff1b
Touseef Younas <touseefyounasch@gmail.com>
2024-08-05 21:04:13 -0400

    Added two new layouts

--- src/Map.js
+++ src/Map.js
@@ -44,17 +44,17 @@ const Map = ({mapContainerRef, orientation, mapData, mapLocation, headline, tagl
     <>
      <div className="flex justify-center items-center w-full h-screen overflow-hidden">
       <div style={{ transform: `scale(${scale})`, transformOrigin: 'center' }}>
-        <div className={`relative ${orientation==='landscape'? landscapeStyle : portraitStyle} bg-white shadow-black shadow-2xl p-4`}>
-        <div className="relative flex h-full w-full border-2 border-black">
+        <div className={`relative ${orientation==='landscape'? landscapeStyle : portraitStyle} bg-white shadow-black shadow-2xl p-2`}>
+        <div className="relative flex h-full w-full">
         <div className="p-1 h-full w-full">
           <div id="map-container" ref={mapContainerRef} className="w-full h-full" />
         </div>
-        <div className={`${labels ? 'block' : 'hidden'} absolute self-end  text-black bg-white w-auto min-w-[240px] left-[50%] px-5 pb-2 my-5`} style={{ transform: 'translate(-50%)' }}> 
-            <div className="block text-center text-[2em] font-bold tracking-wider overflow-hidden z-10">{headline? headline : mapLocation?mapLocation.city: 'New York'}</div>
-            <div className="block divider text-center overflow-hidden">
-                <span className="block-inline relative text-[1em]">{tagline? tagline : mapLocation? mapLocation.country: 'United States'}</span>
+        <div className={`${labels ? 'block' : 'hidden'} absolute self-end text-black bg-white z-10 w-full px-3 pb-2`}>
+            <div className="block text-end text-[1.5em] font-bold font-mono tracking-wider overflow-hidden">{headline? headline : mapLocation?mapLocation.city: 'New York'}</div>
+            <div className="block text-end tracking-wider overflow-hidden">
+                <span className="block-inline relative  text-[0.8em] font-mono">{tagline? tagline : mapLocation? mapLocation.country: 'United States'}</span>
             </div>
-            <div className="text-center text-[0.7em] font-light">{subtitle? subtitle : mapLocation?`${mapLocation.center.lat} / ${mapLocation.center.long}`: '40.7128 °N / 74.0060 °W'}</div>
+            <div className="font-mono text-[0.7em] text-end font-light">{subtitle? subtitle : mapLocation?`${mapLocation.center.lat} / ${mapLocation.center.long}`: '40.7128 °N / 74.0060 °W'}</div>
         </div>
         {/* <div className={`${labels ? 'block' : 'hidden'} absolute self-end  text-white w-full pb-10 px-5 pt-15`}> 
           <div className="block text-center text-[2em] font-bold tracking-wider overflow-hidden z-10">{headline? headline : mapLocation?mapLocation.city: 'New York'}</div>
--- src/utils/LayoutsArray.js
+++ src/utils/LayoutsArray.js
@@ -13,6 +13,29 @@ const layouts = [
         }
     },
     {
-        id: 
+        id: 'block',
+        name: 'block',
+        component: ({headline, tagline, subtitle, mapLocation}) => {
+            <div className={`${labels ? 'block' : 'hidden'} absolute self-end  text-black bg-white w-auto min-w-[240px] left-[50%] px-5 pb-2 my-5`} style={{ transform: 'translate(-50%)' }}> 
+            <div className="block text-center text-[2em] font-bold tracking-wider overflow-hidden z-10">{headline? headline : mapLocation?mapLocation.city: 'New York'}</div>
+            <div className="block divider text-center overflow-hidden">
+                <span className="block-inline relative text-[1em]">{tagline? tagline : mapLocation? mapLocation.country: 'United States'}</span>
+            </div>
+            <div className="text-center text-[0.7em] font-light">{subtitle? subtitle : mapLocation?`${mapLocation.center.lat} / ${mapLocation.center.long}`: '40.7128 °N / 74.0060 °W'}</div>
+        </div>
+        }
+    },
+    {
+        id: 'mono',
+        name: 'mono',
+        component: ({headline, tagline, subtitle, mapLocation}) => {
+            <div className={`${labels ? 'block' : 'hidden'} absolute self-end text-black bg-white z-10 w-full px-3 pb-2`}>
+            <div className="block text-end text-[1.5em] font-bold font-mono tracking-wider overflow-hidden">{headline? headline : mapLocation?mapLocation.city: 'New York'}</div>
+            <div className="block text-end tracking-wider overflow-hidden">
+                <span className="block-inline relative  text-[0.8em] font-mono">{tagline? tagline : mapLocation? mapLocation.country: 'United States'}</span>
+            </div>
+            <div className="font-mono text-[0.7em] text-end font-light">{subtitle? subtitle : mapLocation?`${mapLocation.center.lat} / ${mapLocation.center.long}`: '40.7128 °N / 74.0060 °W'}</div>
+        </div>
+        }
     }
 ]
